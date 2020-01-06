ef73654a302b6e7003f135a6876ef9bc81a8bf7f
hlantian <zxiaoyao313@163.com>
2020-01-06 13:43:28 +0800

    初始化数据

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  BASE_API: '"http://10.41.1.136:8080"'
 })
